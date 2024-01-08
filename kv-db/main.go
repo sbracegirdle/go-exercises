e957b2f1b4708efa5db02ddcd69479a5a8d86575
Simon Bracegirdle <sbracegirdle@gmail.com>
2024-01-08 08:07:16 +0800

    Add http API in front of K/V DB

--- kv-db/main.go
+++ kv-db/main.go
@@ -6,6 +6,7 @@ import (
 	"hash/fnv"
 	"os"
 
+	"github.com/gin-gonic/gin"
 	lru "github.com/hashicorp/golang-lru"
 )
 
@@ -91,10 +92,35 @@ func (kv *KVStore) savePage(hash uint32, page *Page) {
 	encoder.Encode(page)
 }
 
+func startServer(kv *KVStore) {
+	r := gin.Default()
+
+	r.GET("/keys/:key", func(c *gin.Context) {
+		key := c.Param("key")
+		value, ok := kv.Get(key)
+		if ok {
+			c.JSON(200, gin.H{"value": value})
+		} else {
+			c.JSON(404, gin.H{"error": "Key not found"})
+		}
+	})
+
+	r.POST("/keys/:key", func(c *gin.Context) {
+		var body struct {
+			Value string `json:"value"`
+		}
+		if err := c.BindJSON(&body); err != nil {
+			c.JSON(400, gin.H{"error": "Bad request"})
+			return
+		}
+		kv.Set(c.Param("key"), body.Value)
+		c.JSON(200, gin.H{"status": "success"})
+	})
+
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
+
 func main() {
-	// Usage
 	kv := NewKVStore(100, 1000)
-	kv.Set("key", "value")
-	value, _ := kv.Get("key")
-	println(value)
+	startServer(kv)
 }
